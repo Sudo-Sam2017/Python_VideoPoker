9363e5dd3ef8e9719a54a8186795df5cb52da5b6
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2021-10-21 00:08:21 -0400

    Refactored methods for displaying flops. Added checks for two pair, trips, and quads

--- videoPoker.py
+++ videoPoker.py
@@ -1,18 +1,26 @@
 #Video Poker
 
 import random, sys
+from collections import Counter
 
 HEARTS = chr(9829)
 DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
 
+flop = []
+suitList = []
 
 def main():
     print('Welcome User!')
     answer = input("Would you like to play?  ")
     if answer == 'Yes':
-        checkforPair()
+        newFlop = displayFlop()
+        flop = newFlop
+        checkforPair(flop)
+        checkTwoPair(flop)
+        checkThreeOfAKind(flop)
+        checkFourOfAKind(flop)
     else:
         sys.exit()
 
@@ -40,24 +48,68 @@ def removeOneZero(cardList):
         pass
 
 
-def checkforPair():
+def displayFlop():
      flop = []
-     physicalCards = []
+     suitList = []
      deck = buildDeck()
      for x in range(0, 5):
         newCard = deck.pop()
         rank, suit = newCard
         print('You drew a {} of {}.'.format(rank, suit))
         rankNum = rank
+        suitType = suit
         flop += rankNum
-        physicalCards += newCard
+        suitList += suitType
      removeOneZero(flop)
+     return flop
+
+
+def checkforPair(flop):
      cardSet = set(flop)
      containsDuplicates = len(flop) != len(cardSet)
      if containsDuplicates:
          print("You have a pair!")
+         #Error: will not display message when a pair of 10s come on the flop
      else:
          print('No pairs')
+
+def checkTwoPair(flop):
+    doubles = []
+    for k, v in Counter(flop).items():
+        doubles.extend([k] * ( v // 2))
+    numPairs = len(doubles)
+    if numPairs == 2:
+        print("You have two pair!!")
+    else:
+        pass   
+
+def checkThreeOfAKind(flop):
+    triples = []
+    for k, v in Counter(flop).items():
+        triples.extend([k] * (v // 3))
+    numTrips = len(triples)
+    if numTrips >= 1:
+        print("You have three of a kind!")
+    else:
+        pass
+
+def checkFourOfAKind(flop):
+    quads = []
+    for k, v in Counter(flop).items():
+        quads.extend([k] * (v // 4))
+    numQuads = len(quads)
+    if numQuads >= 1:
+        print("You have four of a kind!")
+    else:
+        pass
+
+def checkFlush(suitList):
+    result = all(element == suitList[0] for element in suitList)
+    if (result):
+        print("You have a flush!!")
+    else:
+        pass
+
    
 
 if __name__ == '__main__':
